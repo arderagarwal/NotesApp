2b4b6e99741be4e56e4bf210584b48dac278c8c9
Adarsh Agarwal <adarshagarwal8@gmail.com>
2020-12-14 13:18:00 +0530

    Remove functionality was implemented

--- notes.js
+++ notes.js
@@ -2,7 +2,7 @@ const fs= require('fs')
 const addNotes = function(title,body){
     const notes = getNotes()
     const duplicateNotes = notes.filter(function(note){
-        return title==note.title
+        return title===note.title
     })
     if(duplicateNotes.length===0){
         notes.push({
@@ -29,7 +29,22 @@ const getNotes = function(){
     
 }
 const removeNotes = function(title){
-    console.log('Removing note '+title)
+    const notes = getNotes()
+    if(notes.length === 0){
+        console.log("No notes present")
+    }else{
+            const duplicateNotes = notes.filter(function(note){
+                return title!==note.title
+            })
+            if(duplicateNotes.length===notes.length){
+                console.log("Note with given title not present")
+            }
+            else{
+                fs.writeFileSync('notes.json',JSON.stringify(duplicateNotes))
+                console.log("Note with title: "+ title+" was deleted")
+            }
+    }
+   
 }
 module.exports = {
     getNotes : getNotes,
