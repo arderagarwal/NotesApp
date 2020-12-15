1ec26310b1146c23b1e6a4ce21ed563fcf9375ba
Adarsh Agarwal <adarshagarwal8@gmail.com>
2020-12-15 14:55:40 +0530

    Modified some functions to arrow functions

--- notes.js
+++ notes.js
@@ -2,9 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 const addNotes = function(title,body){
     const notes = getNotes()
-    const duplicateNotes = notes.filter(function(note){
-        return title===note.title
-    })
+    const duplicateNotes = notes.filter( (note) => title===note.title )
     if(duplicateNotes.length===0){
         notes.push({
             title : title,
@@ -34,9 +32,7 @@ const removeNotes = function(title){
     if(notes.length === 0){
         console.log(chalk.inverse.red("No notes present"))
     }else{
-            const duplicateNotes = notes.filter(function(note){
-                return title!==note.title
-            })
+            const duplicateNotes = notes.filter((note) => title!==note.title)
             if(duplicateNotes.length===notes.length){
                 console.log(chalk.inverse.red("Note with given title not present"))
             }
